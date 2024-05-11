ed4bd8e1d3f8aa1b720fd219638020aefbe189b5
AwaHardika <awalrafihardika@gmail.comm>
2024-05-11 10:42:35 +0700

    perubahan footer lagi

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
     <div className='w-screen h-[300px] mt-12 flex justify-center items-center flex-col bg-slate-300'>
     
     <div className='w-full flex gap-4 justify-center'>
-    <img src={facebook} alt=""  className='md:w-[100px] md:h-100px w-[50px] h-[50px]'/>
-    <img src={linkedln} alt="" className='md:w-[100px] md:h-100px w-[50px] h-[50px]' />
-    <img src={instagram} alt="" className='md:w-[100px] md:h-100px w-[50px] h-[50px]'/>
-    <img src={youtube} alt="" className='md:w-[100px] md:h-100px w-[50px] h-[50px]'/>
+    <img src={facebook} alt=""  className='md:w-[100px] md:h-[100px] w-[50px] h-[50px]'/>
+    <img src={linkedln} alt="" className='md:w-[100px] md:h-[100px] w-[50px] h-[50px]' />
+    <img src={instagram} alt="" className='md:w-[100px] md:h-[100px] w-[50px] h-[50px]'/>
+    <img src={youtube} alt="" className='md:w-[100px] md:h-[100px] w-[50px] h-[50px]'/>
     </div>
     <h1>Copyright by Awal Rafi Hardika @2023</h1>
       
